f39a00538895df99a74a1c27daa98b2271fcdb53
Hunter Isaacs <hsiman335@outlook.com>
2023-11-02 13:36:07 -0700

    trying to get it to work.

---
+++ src/Com/BlankSlot.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import FirstWins from './FirstWins';
+import SecondWins from './SecondWins';
+import ThirdWins from './ThirdWins';
+import FourthWins from './FourthWins';
+import OvertimeWins from './OTWinsChart';
+import All from './AllPie';
+import OverAll from './OverAllBest';
+import TotalPoints from './TotalPoints';
+import OTClutchPer from './OTClutch';
+import './BlankSlot.css';
+
+
+function BlankSlot({ data, chartType }) {
+  const renderChart = () => {
+    switch (chartType) {
+      case 'firstPlaceWins':
+        return <FirstWins data={data} />;
+      case 'secondPlaceWins':
+        return <SecondWins data={data} />;
+      case 'thirdPlaceWins':
+        return <ThirdWins data={data} />;
+      case 'fourthPlaceWins':
+        return <FourthWins data={data} />;
+      case 'overtimeWins':
+        return <OvertimeWins data={data} />;
+      case 'everybodysStats':
+        return <All data={data} />;
+      case 'PointsPerGame':
+        return <OverAll data={data} />;
+      case 'TotalPoints':
+        return <TotalPoints data={data} />;
+      case 'OTClutch':
+        return <OTClutchPer data={data} />;
+      default:
+        return <p>Blank Area</p>;
+    }
+  };
+
+  return (
+    <div className="blank-slot">
+      <div className="blank-slot-content">
+      {renderChart()}
+      </div>
+    </div>
+  );
+}
+
+export default BlankSlot;
---
+++ src/Com/Logo.js
@@ -0,0 +1,11 @@
+// Logo.js
+import React from 'react';
+import logoImage from './favicon.png';
+
+function Logo() {
+  return (
+    <img src={logoImage} alt="Logo" width="50" height="50" />
+  );
+}
+
+export default Logo;
